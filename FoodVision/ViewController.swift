e26a6537707a66268145fd422633510027543dfd
Sekaye <sekaye@gmail.com>
2022-09-29 19:54:26 -0700

    initial commit

---
+++ FoodVision/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodVision
+//
+//  Created by Sekaye Knutson on 9/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
